4b3aa928b9af6b6a0221f3e2d92cf6774007693f
qujsh-home <ningmingqaz@126.com>
2017-04-10 23:28:30 +0800

    Signed-off-by: qujsh-home <ningmingqaz@126.com>

--- src/components/member/publish.vue
+++ src/components/member/publish.vue
@@ -17,18 +17,21 @@
                             </el-option>
                         </el-select>
                     </el-form-item>
-                </el-form>
 
-                <el-upload
-                        class="upload-demo"
-                        drag
-                        action="http://localhost:9090/videos/upload"
-                        :thumbnail-mode="true"
+                    <el-form-item label="封面" prop="v_pic">
+                        <el-upload
+                                class="upload-demo"
+                                drag
+                                action="http://localhost:9090/videos/upload"
+
                         >
-                    <i class="el-icon-upload"></i>
-                    <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-                    <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
-                </el-upload>
+                            <i class="el-icon-upload"></i>
+                            <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+                            <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+                        </el-upload>
+                    </el-form-item>
+
+                </el-form>
 
             </el-col>
             <el-col :span="12">
