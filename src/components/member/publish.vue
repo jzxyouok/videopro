cbef27b33d25b8896eaa6d47275df6731a809e8b
qujsh-home <ningmingqaz@126.com>
2017-04-11 23:18:00 +0800

    Signed-off-by: qujsh-home <ningmingqaz@126.com>

--- src/components/member/publish.vue
+++ src/components/member/publish.vue
@@ -23,7 +23,7 @@
                                 class="upload-demo"
                                 drag
                                 action="http://localhost:9090/videos/upload"
-
+                                name="Uploader[imageFile]"
                         >
                             <i class="el-icon-upload"></i>
                             <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
