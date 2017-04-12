8817c0c99f63610b74fa64cba1a8e4929788b6aa
ningming <ningm@superwan.com>
2017-04-12 18:13:14 +0800

    Signed-off-by: ningming <ningm@superwan.com>

--- src/components/member/publish.vue
+++ src/components/member/publish.vue
@@ -24,6 +24,12 @@
                                 drag
                                 action="http://localhost:9090/videos/upload"
                                 name="Uploader[imageFile]"
+                                :on-preview="handlePreview"
+                                :on-remove="handleRemove"
+                                :before-upload="handleBeforeUpload"
+                                :on-success="handleSuccess"
+                                :multiple="false"
+                                :file-list="fileList"
                         >
                             <i class="el-icon-upload"></i>
                             <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
@@ -38,13 +44,24 @@
 
             </el-col>
         </el-row>
+
+        <el-dialog title="图片预览" v-model="dialogVisible" size="small">
+            <span>
+                <img :src="video.v_pic.url"/>
+            </span>
+              <span slot="footer" class="dialog-footer">
+                <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+              </span>
+        </el-dialog>
+
     </div>
 </template>
 <style>
     h3{color:#5e6d82;margin-bottom: 10px}
+    li.el-upload-list__item.is-success{height: 25px;}
 </style>
 <script>
-
+    //这儿的css真心写不来啊，我个擦擦
     export default
     {
         mounted()
@@ -56,13 +73,50 @@
                 video: {
                     v_id:0,
                     v_title:'',
-                    v_class:''
-                }
+                    v_class:'',
+                    v_pic:{
+                        name:"",
+                        url:""
+                    }
+                },
+                dialogVisible: false,
+                fileList: []
             };
         },
         methods:{
             alertClass(){
                 //alert(this.video.v_class);
+            },
+            handlePreview(file){
+                //console.log(file);
+                //预览
+                this.dialogVisible=true;
+                //console.log(this.fileList);
+            },
+            handleRemove(file, fileList){
+                //移除图片
+                this.video.v_pic.url="";
+                this.video.v_pic.name="";
+            },
+            handleBeforeUpload(file){   //上传文件之前，用来判断文件格式，大小等
+
+            },
+            handleSuccess(response, file, fileList){    //只要没报错，即使没得到结果，就都是成功的设置
+                //console.log(response);
+                //console.log(file.name);
+                //响应成功
+                if(response.status==1)
+                {
+                    this.video.v_pic.url=response.url;
+                    this.video.v_pic.name=file.name;
+
+                    this.fileList.splice(0, 1, this.video.v_pic);
+
+                }
+                else
+                {
+                    alert("上传失败，请稍后再试");
+                }
             }
         }
     }
